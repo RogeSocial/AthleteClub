5c9323f3893c786a61c1f795300c5df780617635
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-12-19 20:51:46 +0100

    fixed basic routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
+import HomePage from './pages/HomePage.tsx'
+import App from './App';
+import { 
+  Route, 
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements } from 'react-router-dom';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route index element={<HomePage/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
