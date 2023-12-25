6beee2f517a38c64574cf488732862b31405aaff
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-12-25 18:18:44 +0100

    made a account route and a post method

---
+++ server/routes/accountRoute.js
@@ -0,0 +1,36 @@
+import Router from "express";
+export const accountRouter = Router();
+
+import mongoose, { Schema } from ' mongoose';
+
+import crypto from 'crypto';
+const salt = "paraplane".toString('hex');
+
+function getHash(password) {
+    let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`)
+    return hash;
+}
+
+const accountSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean, default: false }
+})
+
+mongoose.model('accounts', accountSchema);
+
+accountRouter.post('/', async (req, res) => {
+    try {
+        const account = new mongoose.models.accounts();
+        account.email = req.body.email;
+        account.password = getHash(req.body.password);
+        await account.save();
+        res.json({"result": "created"});
+    } catch (error) {
+        if (error.email === 'MongoError' && error.code === 11000) {
+            res.status(400).json({ "error": "email already exist!" });
+        } else {
+            res.status(500).json({ "error": "internal server error" });
+        }
+    }
+})
